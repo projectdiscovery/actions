af07c34a6bed9b69052d0568b3a4d518bfc0ca74
Dwi Siswanto <git@dw1.io>
2024-08-02 18:09:57 +0700

    feat: add setup/nuclei
    Signed-off-by: Dwi Siswanto <git@dw1.io>

---
+++ setup/nuclei/main.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function locateCmd() {
+    which $1 >/dev/null || {
+        echo "::error::Could not find $1"
+        exit 1
+    }
+}
+
+locateCmd "go"
+
+install=$(go install -v "github.com/projectdiscovery/nuclei/v3/cmd/nuclei@${VERSION}")
+
+locateCmd "nuclei"
+
+echo "::debug::Getting Nuclei version"
+get_version=$(nuclei --version 2>&1)
+
+version=$(echo "${get_version}" | awk '/Nuclei Engine Version:/ {print $NF}')
+config_dir=$(echo "${get_version}" | awk '/Nuclei Config Directory:/ {print $NF}')
+cache_dir=$(echo "${get_version}" | awk '/Nuclei Cache Directory:/ {print $NF}')
+pdcp_dir=$(echo "${get_version}" | awk '/PDCP Directory:/ {print $NF}')
+
+{
+  echo "version=${version}"
+  echo "config_dir=${config_dir}"
+  echo "cache_dir=${cache_dir}"
+  echo "pdcp_dir=${pdcp_dir}"
+} >> $GITHUB_OUTPUT
