f83855aa93e16575a00115dfd5cfbbe01e800fdf
Dwi Siswanto <git@dw1.io>
2024-08-04 09:41:04 +0700

    fix(commit): handles non-zero exit when add/commit
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- commit/main.sh
+++ commit/main.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-set -euo pipefail
-
 echo "::debug::Remove any trailing newlines from input files"
 FILES="$(echo -n "${FILES}")"
 
 echo "::debug::$(wc -l <<< "${FILES}") files to commit"
 
 while IFS= read -r file; do
-    git add "${file}"
+    add=$(git add "${file}" 2>&1)
+    [[ $? -gt 0 ]] && echo "::warning::${add}"
 done <<< "${FILES}"
 
-git commit -m "${MESSAGE}"
+commit=$(git commit -m "${MESSAGE}")
+[[ $? -gt 0 ]] && echo "::warning::Could not create commit: $(tail -n 1 <<< "${commit}")"
