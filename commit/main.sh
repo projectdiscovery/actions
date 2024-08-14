145e2af8c2cc4fbcee27ebba51ced96861afd7fb
Dwi Siswanto <git@dw1.io>
2024-08-14 08:45:54 +0700

    feat(commit): print output as debug
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- commit/main.sh
+++ commit/main.sh
@@ -8,7 +8,9 @@ echo "::debug::$(wc -l <<< "${FILES}") files to commit"
 while IFS= read -r file; do
     add=$(git add "${file}" 2>&1)
     [[ $? -gt 0 ]] && echo "::warning::${add}"
+    echo "::debug::${add}"
 done <<< "${FILES}"
 
 commit=$(git commit -m "${MESSAGE}" 2>&1)
 [[ $? -gt 0 ]] && echo "::warning::Could not create commit: $(tail -n 1 <<< "${commit}")"
+echo "::debug::${commit}"
