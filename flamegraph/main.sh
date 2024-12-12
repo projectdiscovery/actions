a448b4b952028591e15309bdce708aa290f868ea
Dwi Siswanto <25837540+dwisiswant0@users.noreply.github.com>
2024-12-13 06:51:33 +0700

    feat: add flamegraph action (#7)
    Signed-off-by: Dwi Siswanto <git@dw1.io>

---
+++ flamegraph/main.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function locateCmd() {
+    which $1 >/dev/null || {
+        echo "::error::Could not find $1"
+        exit 1
+    }
+}
+
+locateCmd "curl"
+locateCmd "jq"
+
+INPUT_PROF=$(eval realpath "${INPUT_PROF}")
+BASE_INPUT_PROF=$(basename "${INPUT_PROF}")
+
+upload=$(curl --data-binary "@${INPUT_PROF}" "https://flamegraph.com/?name=${INPUT_NAME}&file_name=${BASE_INPUT_PROF}")
+
+flamegraph_key=$(jq -r '.key | walk(if . == null then "" else . end)' <<< "${upload}")
+flamegraph_url=$(jq -r '.url | walk(if . == null then "" else . end)' <<< "${upload}")
+flamegraph_message=$(jq -r '.message | walk(if . == null then "" else . end)' <<< "${upload}")
+
+{
+    echo "key=${flamegraph_key}"
+    echo "url=${flamegraph_url}"
+    echo "message=${flamegraph_message}"
+} >> $GITHUB_OUTPUT
+
+if [[ -n "${flamegraph_message}" ]]; then
+    echo "::error::${flamegraph_message}"
+    exit 1
+fi
