11a8e43b25fea25abd5357bf0e131aa897b34aa9
Dwi Siswanto <git@dw1.io>
2024-12-18 08:13:28 +0700

    ci(action-docs): update placeholders
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- .github/scripts/action-docs.sh
+++ .github/scripts/action-docs.sh
@@ -11,11 +11,15 @@ function generateDocs() {
 	local src="${1}"
 	local dir="$(dirname "${1}")"
 	local doc="${dir}/README.md"
+	local tag="$(git describe --tags --abbrev=0 | cut -d '.' -f1)"
 
 	# rm if exists
 	[[ -f "${doc}" ]] && rm -rf "${doc}"
-
 	action-docs --no-banner --source "${src}" > "${doc}"
+
+	# replace placeholders
+	sed -i -e "s|\*\*\*PROJECT\*\*\*|${GITHUB_REPOSITORY}/${dir##./}|g" "${doc}"
+	sed -i -e "s|\*\*\*VERSION\*\*\*|${tag}@g" "${doc}"
 
 	[[ "$(git status -s | wc -l)" -gt "0" ]] && commit "${dir}"
 }
