273d4098f5596dad42267edd1705013a9667917a
Dwi Siswanto <25837540+dwisiswant0@users.noreply.github.com>
2024-12-20 13:59:42 +0700

    refactor(nuclei): download w/ javascript & bun (#15)
    * refactor(nuclei): download w/ javascript & bun
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * chore(dependabot): add `npm` ecosystem
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    ---------
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>

---
+++ setup/nuclei/download.js
@@ -0,0 +1,59 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+const tc = require('@actions/tool-cache')
+
+module.exports = async ({version, token}) => {
+  const path = require('path')
+  const os = process.env.RUNNER_OS === "macOS" || process.env.RUNNER_OS.toLowerCase()
+  const arch = process.env.RUNNER_ARCH.toLowerCase()
+    .replace('x86', '386')
+    .replace('x64', 'amd64')
+  const repo = {owner: 'projectdiscovery', repo: 'nuclei'}
+  const octokit = github.getOctokit(token)
+
+  if (version === 'latest') {
+    const latestRelease = await octokit.rest.repos.getLatestRelease(repo)
+    version = latestRelease.data.tag_name
+  } else {
+    try {
+      await octokit.rest.repos.getReleaseByTag({...repo, tag: version})
+    } catch (error) {
+      core.setFailed(`Could not get "${version}" tag: ${error.message}`)
+      process.exit(1)
+    }
+  }
+
+  const asset = `nuclei_${version.replace(/^v/, '')}_${os}_${arch}.zip`
+  const assetURL = `https://github.com/${repo.owner}/${repo.repo}/releases/download/${version}/${asset}`
+
+  core.debug(`Checking cache for nuclei...`)
+  let nucleiPath = tc.find('nuclei', version, arch)
+
+  if (!nucleiPath) {
+    nucleiPath = path.join(process.env.GITHUB_WORKSPACE, '../', 'nuclei')
+
+    let zipPath
+    try {
+      core.debug(`Downloading nuclei ${version} for ${os}/${arch}...`)
+      zipPath = await tc.downloadTool(assetURL)
+    } catch (error) {
+      core.setFailed(`Could not download nuclei: ${error.message}`)
+      process.exit(1)
+    }
+
+    let extractedPath
+    try {
+      core.debug('Extracting nuclei...')
+      extractedPath = await tc.extractZip(zipPath, nucleiPath)
+    } catch (error) {
+      core.setFailed(`Could not extract nuclei: ${error.message}`)
+      process.exit(1)
+    }
+
+    core.debug('Caching nuclei...')
+    nucleiPath = await tc.cacheDir(extractedPath, 'nuclei', version, arch)
+  }
+
+  core.debug('Adding nuclei to PATH...')
+  core.addPath(nucleiPath)
+}
