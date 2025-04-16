4ec67a74c48d1e9b46086f8d80d96f9b1e202139
Dwi Siswanto <git@dw1.io>
2025-04-17 04:56:51 +0700

    feat: add svu-next action
    Signed-off-by: Dwi Siswanto <git@dw1.io>

---
+++ svu-next/main.js
@@ -0,0 +1,83 @@
+const core = require('@actions/core')
+const exec = require('@actions/exec')
+const github = require('@actions/github')
+const context = JSON.parse(process.env.GITHUB_CONTEXT)
+const currentTag = process.env.CURRENT_TAG.trim()
+const token = process.env.INPUT_TOKEN
+const always = (process.env.INPUT_ALWAYS === 'true')
+const logDirectory = process.env.INPUT_LOG_DIRECTORY
+const metadata = process.env.INPUT_METADATA
+const prerelease = process.env.INPUT_PRERELEASE
+const tagMode = process.env.INPUT_TAG_MODE
+const tagPattern = process.env.INPUT_TAG_PATTERN
+const tagPrefix = process.env.INPUT_TAG_PREFIX
+const v0 = (process.env.INPUT_V0 === 'true')
+
+try {
+  core.debug("Setting up Octokit...")
+  const octokit = github.getOctokit(token)
+
+  let nextVersion = ''
+  let nextVersionErr = ''
+  const nextVersionOptions = {}
+  nextVersionOptions.listeners = {
+    stdout: (data) => nextVersion += data.toString(),
+    stderr: (data) => nextVersionErr += data.toString()
+  }
+  await exec.exec('svu', [
+    'next',
+    `${always ? '--always' : ''}`,
+    '--log-directory',
+    `"${logDirectory}"`,
+    '--metadata',
+    `"${metadata}"`,
+    '--prerelease',
+    `"${prerelease}"`,
+    '--tag-mode',
+    `"${tagMode}"`,
+    '--tag-pattern',
+    `"${tagPattern}"`,
+    '--tag-prefix',
+    `"${tagPrefix}"`,
+    `${v0 ? '--v0' : ''}`,
+  ], nextVersionOptions)
+
+  if (nextVersionErr) throw new Error(`Error: ${nextVersionErr}`);
+  nextVersion = nextVersion.trim()
+  core.info(`next version tag: ${nextVersion}`)
+
+  if (nextVersion === currentTag) {
+    core.info(`Current tag is already ${nextVersion}, skipping...`)
+    core.setOutput('tag', currentTag)
+    return
+  }
+
+  core.info(`Creating and pushing tag ${nextVersion}...`)
+  const [owner, repo] = context.repository.split('/')
+  const tagMessage = `Version ${nextVersion}`
+  const tag = await octokit.rest.git.createTag({
+    owner: owner,
+    repo: repo,
+    tag: nextVersion,
+    message: tagMessage,
+    object: context.sha,
+    type: 'commit',
+    tagger: {
+      name: context.triggering_actor,
+      email: `${context.actor_id}+${context.triggering_actor}@users.noreply.github.com`,
+    }
+  })
+
+  await octokit.rest.git.createRef({
+    owner: owner,
+    repo: repo,
+    ref: `refs/tags/${nextVersion}`,
+    sha: tag.data.sha
+  })
+
+  core.info(`Tag ${nextVersion} created and pushed successfully.`)
+  core.setOutput('tag', nextVersion)
+} catch (error) {
+  core.setFailed(error.message)
+  process.exit(1)
+}
