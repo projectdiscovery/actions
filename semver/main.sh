aeffad39af333a606c11c738ca7128098bfabdc4
Dwi Siswanto <git@dw1.io>
2025-04-30 13:27:02 +0700

    feat(semver): adds `allow-no-tags` input
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- semver/main.sh
+++ semver/main.sh
@@ -4,12 +4,13 @@ source <(curl -fsSL "https://github.com/projectdiscovery/actions/raw/refs/heads/
 source <(curl -fsSL "https://github.com/cloudflare/semver_bash/raw/refs/heads/master/semver.sh")
 
 TAG="${INPUT_TAG}"
+ALLOW_NO_TAGS="${INPUT_ALLOW_NO_TAGS:-false}"
 
 if [[ -z "$TAG" ]]; then
     TAG="$(git describe --tags --abbrev=0)"
 fi
 
-if [[ -z "$TAG" ]]; then
+if [[ -z "$TAG" && "${ALLOW_NO_TAGS}" != "true" ]]; then
     printErrorWithExit "Could not find a tag in the current branch. Please set the input 'tag'." 1
 fi
 
@@ -18,8 +19,10 @@ MINOR=0
 PATCH=0
 SPECIAL=""
 
-semverParseInto $TAG MAJOR MINOR PATCH SPECIAL
-printNotice "$TAG -> M: $MAJOR m:$MINOR p:$PATCH s:$SPECIAL"
+if [[ -n "$TAG" ]]; then
+    semverParseInto $TAG MAJOR MINOR PATCH SPECIAL
+    printNotice "$TAG -> M: $MAJOR m:$MINOR p:$PATCH s:$SPECIAL"
+fi
 
 {
     echo "major=${MAJOR}"
