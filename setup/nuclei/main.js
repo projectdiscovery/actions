845ddd98a53beff778ead393ca269712167d1dd2
Dwi Siswanto <25837540+dwisiswant0@users.noreply.github.com>
2025-09-30 20:53:48 +0700

    feat(nuclei): add check for existing nuclei bin (#42)
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- setup/nuclei/main.js
+++ setup/nuclei/main.js
@@ -1,7 +1,18 @@
 const dl = require('./download')
 const core = require('@actions/core')
+const exec = require('@actions/exec')
 const token = process.env.INPUT_TOKEN
 const version = process.env.INPUT_VERSION
+
+try {
+  const command = process.platform === 'win32' ? 'where' : 'which'
+  await exec.exec(command, ['nuclei'], { silent: true })
+
+  core.info('nuclei is already installed and available in PATH')
+  process.exit(0)
+} catch (_) {
+  // nuclei not found in PATH, continue with installation
+}
 
 if (version !== 'latest' && !version.match(/^v\d+\.\d+\.\d+$/)) {
   core.setFailed('Version must be "latest" or in format "vX.Y.Z"')
